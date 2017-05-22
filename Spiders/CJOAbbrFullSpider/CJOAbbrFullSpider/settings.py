822a2b2a33ca3c074c1b42915f74a61599fddd63
lxw0109 <lxw0109@gmail.com>
2017-05-22 23:48:42 +0800

    crawl 3328 companies' case counts

--- Spiders/CJOAbbrFullSpider/CJOAbbrFullSpider/settings.py
+++ Spiders/CJOAbbrFullSpider/CJOAbbrFullSpider/settings.py
@@ -22,7 +22,7 @@ NEWSPIDER_MODULE = 'CJOAbbrFullSpider.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 1
+CONCURRENT_REQUESTS = 6
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
